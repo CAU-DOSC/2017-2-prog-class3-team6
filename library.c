bc556b74f6f1789650a5df25f8924c52cb40e8d0
soongle2 <skql5810@gmail.com>
2017-11-27 15:02:41 +0900

    add 3functions on library.c

--- library.c
+++ library.c
@@ -4,3 +4,37 @@
   먼저 readme.txt를 읽어주세요
                               */
 #include "header.h"
+node *CreatNode(int *data,int *count)
+{
+	node *cur;
+	cur=(node*)malloc(sizeof(node));
+	if(cur==NULL){
+		retrun NULL;
+	}
+	cur->value=data;
+	cur->next=NULL;
+	(*count)++;
+	return cur;
+}
+node *AddNode(node* cur,node* head)
+{
+	node* nextnode=head;
+	if(head==NULL){
+		head=cur;
+		return head;
+	}
+while(nextnode->next!=NULL){
+	nextnode=nextnode->next;
+}
+nextnode->next=cur;
+return head;
+}
+void PrintNode(node* head)
+{
+	node* nextnode = head;
+	while (nextnode != NULL)
+	{
+		printf("%-4d", nextnode->value);
+		nextnode = nextnode->next;
+	}
+}
--- main.c
+++ main.c
@@ -7,11 +7,17 @@
 #include<stdlib.h>
 #include"header.h"
 int main(){
-	node *head,*cur,*tail;
-	//노드의 머리,현재 위치의 노드,노드의 꼬리
-	int date=0;
+	node *head=NULL;
+	node *cur=NULL;
+	//노드의 머리,현재 위치의 노드
+	int data=0;
 	//node의 value에 넣을 정수 값
 	int count=0;
 	//노드의 갯수를 카운트해주는 변수
+	printf("정수를 입력해주세요\n");
+	while(scanf("%d",&data)!=EOF){
+		cur=CreatNode(data,&count);
+		head=AddNode(cur,head);
+	}
 	return 0;
 }
